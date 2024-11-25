d56a45b6812808b097a4a0565e512bf0549fecf3
Jayde Fanjoy <Jayde.Fanjoy@gmail.com>
2024-11-25 15:50:40 -0400

    Added basic unit tests for homepage

---
+++ RailWikiChangeListener/spec/home_spec.rb
@@ -0,0 +1,51 @@
+# spec/home_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe 'User is an admin', type: :system do
+  before do
+    @user = User.create(
+      email: 'test@example.com', 
+      password: 'password123',
+      password_confirmation: 'password123',
+      role: :admin # Adjust depending on how roles are defined
+    )
+  end
+
+  it 'visits the landing page' do
+    # Sign in the user
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: 'password123'
+    click_button 'Log in'
+
+    # Test landing page
+    visit root_path
+    expect(page).to have_content('You have successfully authenticated.')
+    expect(page).to have_content('Listener Control')
+  end
+end
+
+RSpec.describe 'User is a user', type: :system do
+  before do
+    @user2 = User.create!(
+      email: 'test2@example.com', 
+      password: 'password123',
+      password_confirmation: 'password123',
+      role: :user # Adjust depending on how roles are defined
+    )
+  end
+
+  it 'visits the landing page' do
+    # Sign in the user
+    visit new_user_session_path
+    fill_in 'Email', with: @user2.email
+    fill_in 'Password', with: 'password123'
+    click_button 'Log in'
+
+    # Test landing page
+    visit root_path
+    expect(page).to have_content('You have successfully authenticated.')
+    expect(page).to_not have_content('Listener Control')
+  end
+end
