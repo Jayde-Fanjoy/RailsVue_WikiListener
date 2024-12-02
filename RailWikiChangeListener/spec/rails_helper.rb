6e9edec6ec0671f06089213f809adc09354022e0
Jayde Fanjoy <Jayde.Fanjoy@gmail.com>
2024-12-02 16:15:10 -0400

    Adding selenium as a gem.

--- RailWikiChangeListener/spec/rails_helper.rb
+++ RailWikiChangeListener/spec/rails_helper.rb
@@ -7,6 +7,21 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'capybara/rspec'
+require 'selenium/webdriver'
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless') # Run headless
+  options.add_argument('--disable-gpu') # Disable GPU acceleration
+  options.add_argument('--no-sandbox') # Required for Docker
+  options.add_argument('--disable-dev-shm-usage') # Prevent resource issues in CI
+  options.add_argument('--remote-debugging-port=9222') # Debugging port
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :selenium_chrome_headless
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
